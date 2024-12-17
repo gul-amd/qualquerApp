40a2a6bda02e57674f63feab104fab24783f63d0
Gulamo <gulamo.amd@gmail.com>
2024-12-17 14:59:52 +0200

    chore: criacao de um contador.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,65 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <div class="text-center">
+      <h1>Contador</h1>
+      <p>Este e um contador!</p>
+
+      <br />
+      <br />
+      <br />
+
+      <!-- Botão redondo -->
+      <q-btn
+        icon="add"
+        color="primary"
+        round
+        size="lg"
+        @click="incrementarContador"
+        class="q-mt-md"
+      />
+      <br />
+      <br />
+      <br />
+      <br />
+      <p class="q-mt-md">Pressione o botao acima para contar!</p>
+      <p class="q-mt-md">
+        <strong
+          ><h2>{{ contador }}</h2></strong
+        >
+      </p>
+      <!-- Botão de reset -->
+      <q-btn
+        label="Resetar Contagem"
+        color="red"
+        flat
+        @click="resetarContador"
+        class="q-mt-md q-ml-md"
+      />
+    </div>
   </q-page>
 </template>
 
-<script setup>
-//
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      contador: 0, // Estado inicial do contador
+    }
+  },
+  methods: {
+    incrementarContador() {
+      this.contador++ // Incrementa o contador a cada clique
+    },
+    resetarContador() {
+      this.contador = 0 // Reseta o contador para zero
+    },
+  },
+}
 </script>
+
+<style scoped>
+h1 {
+  color: #000;
+}
+</style>
